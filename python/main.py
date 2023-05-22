201db88920c3302cf3f756e8be382a88e3ed92fc
Emerson Herzeg <emer.herzeg@gmail.com>
2023-05-22 11:49:42 -0300

    Ajustes do do caminho do modelo

--- python/main.py
+++ python/main.py
@@ -4,7 +4,7 @@ import joblib
 import numpy as np
 
 # Carrega o modelo treinado
-model = joblib.load('/home/herzeg/univesp/pi4/python/modelo_treinado_random_forest.pkl')
+model = joblib.load('models/modelo_treinado_random_forest.pkl')
 
 # Recupera os argumentos de entrada da linha de comando
 ano, mes, dia, hora, minuto, bme280_hum, bme280_pres, bme280_temp = map(float, sys.argv[1:])
